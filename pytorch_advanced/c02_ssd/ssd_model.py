1b29db538613721447e1da86aba7063ff6ebf93e
Shota Shimizu <shota_shimizu@caddi.jp>
2022-01-15 16:46:35 +0900

    Add SSD

--- pytorch_advanced/c02_ssd/ssd_model.py
+++ pytorch_advanced/c02_ssd/ssd_model.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from pathlib import Path
 import random
 import xml.etree.ElementTree as ET
+import logging
 
 import cv2
 import matplotlib.pyplot as plt
@@ -29,6 +30,8 @@ from .data_augumentation import (
     SubtractMeans,
 )
 from .match import match
+
+logger = logging.getLogger(__name__)
 
 torch.manual_seed(1234)
 np.random.seed(1234)
@@ -253,3 +256,501 @@ def od_collate_fn(batch):
     # n は画像ごとに異なり、画像内にある物体の数
     # 5 は [xmin, ymin, xmax, ymax, class_index]
     return imgs, targets
+
+
+def make_vgg():
+    """Make the vgg layers"""
+    layers = []
+    in_channels = 3
+
+    # channels
+    cfg = [
+        64,
+        64,
+        "M",
+        128,
+        128,
+        "M",
+        256,
+        256,
+        256,
+        "MC",
+        512,
+        512,
+        512,
+        "M",
+        512,
+        512,
+        512,
+    ]
+
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif v == "MC":
+            # max ceil, default is max floor
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+
+    pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+    conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)
+    conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
+    layers += [pool5, conv6, nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
+    return nn.ModuleList(layers)
+
+
+def make_extras():
+    """8層の extras layer"""
+    layers = []
+    in_channels = 1024
+
+    # extras cfg
+    cfg = [256, 512, 128, 256, 128, 256, 128, 256]
+
+    layers += [nn.Conv2d(in_channels, cfg[0], kernel_size=1)]
+    layers += [nn.Conv2d(cfg[0], cfg[1], kernel_size=3, stride=2, padding=1)]
+    layers += [nn.Conv2d(cfg[1], cfg[2], kernel_size=1)]
+    layers += [nn.Conv2d(cfg[2], cfg[3], kernel_size=3, stride=2, padding=1)]
+    layers += [nn.Conv2d(cfg[3], cfg[4], kernel_size=1)]
+    layers += [nn.Conv2d(cfg[4], cfg[5], kernel_size=3)]
+    layers += [nn.Conv2d(cfg[5], cfg[6], kernel_size=1)]
+    layers += [nn.Conv2d(cfg[6], cfg[7], kernel_size=3)]
+    # relu within SSD
+    return nn.ModuleList(layers)
+
+
+# def to_print(i):
+#    return f"layers += [nn.Conv2d(cfg[{i}], cfg[{i+1}], kernel_size=3)]"
+
+
+def make_loc_conf(num_classes=21, bbox_aspect_num=None):
+    """デフォルトボックスのオフセットを出力する loc_layers
+    デフォルトボックスに対する各クラスの信頼度　confidence を出力する　conf_layers を作成する
+
+    Returns:
+    """
+    if bbox_aspect_num is None:
+        bbox_aspect_num = [4, 6, 6, 6, 4, 4]
+    loc_layers = []
+    conf_layers = []
+
+    # VGG-22, conv4_3 (source1) の conv
+    loc_layers += [nn.Conv2d(512, bbox_aspect_num[0] * 4, kernel_size=3, padding=1)]
+    conf_layers += [
+        nn.Conv2d(512, bbox_aspect_num[0] * num_classes, kernel_size=3, padding=1)
+    ]
+
+    # vgg の最終層 (source2)に対する畳み込み層
+    loc_layers += [nn.Conv2d(1024, bbox_aspect_num[1] * 4, kernel_size=3, padding=1)]
+    conf_layers += [
+        nn.Conv2d(1024, bbox_aspect_num[1] * num_classes, kernel_size=3, padding=1)
+    ]
+
+    # extra の (source3)に対する畳み込み層
+    loc_layers += [nn.Conv2d(512, bbox_aspect_num[2] * 4, kernel_size=3, padding=1)]
+    conf_layers += [
+        nn.Conv2d(512, bbox_aspect_num[2] * num_classes, kernel_size=3, padding=1)
+    ]
+
+    # extra の (source4)に対する畳み込み層
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[3] * 4, kernel_size=3, padding=1)]
+    conf_layers += [
+        nn.Conv2d(256, bbox_aspect_num[3] * num_classes, kernel_size=3, padding=1)
+    ]
+
+    # extra の (source5)に対する畳み込み層
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[4] * 4, kernel_size=3, padding=1)]
+    conf_layers += [
+        nn.Conv2d(256, bbox_aspect_num[4] * num_classes, kernel_size=3, padding=1)
+    ]
+
+    # extra の (source6)に対する畳み込み層
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[5] * 4, kernel_size=3, padding=1)]
+    conf_layers += [
+        nn.Conv2d(256, bbox_aspect_num[5] * num_classes, kernel_size=3, padding=1)
+    ]
+
+    return nn.ModuleList(loc_layers), nn.ModuleList(conf_layers)
+
+
+def to_print(i):
+    print(f"# extra の (source{i+1}に対する畳み込み層")
+    print(
+        f"loc_layers += [nn.Conv2d(512, bbox_aspect_num[{i}] * 4, kernel_size=3, padding=1)]"
+    )
+    print(
+        f"conf_layers += [nn.Conv2d(1024, bbox_aspect_num[{i}] * 4, kernel_size=3, padding=1)]"
+    )
+
+
+class L2Norm(nn.Module):
+    def __init__(self, input_channels=512, scale=20):
+        """convC4_3からの出力をscale=20のL2Norm で正規化する層
+
+        Args:
+            input_channels:
+            scale:
+        """
+        super(L2Norm, self).__init__()
+        self.weight = nn.Parameter(torch.Tensor(input_channels))
+        self.scale = scale
+        self.reset_parameters()
+        self.eps = 1e-10
+
+    def reset_parameters(self):
+        """set all weights to `scale`"""
+        init.constant_(self.weight, self.scale)
+
+    def forward(self, x):
+        """38*38 features, take sum of square for 512 channels
+        use the 38*38 values to normalize features and mul the weights
+
+        Args:
+            x:
+
+        Returns:
+        """
+        # norm will be torch.Size([batch_num, 1, 38, 38])
+        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt() + self.eps
+        x = torch.div(x, norm)
+
+        # mul the weights: torch.Size([512])
+        # result is torch.Size([batch_num, 512, 38, 38])
+        weights = self.weight.unsqueeze(0).unisqueeze(2).unsqueeze(3).expand_as(x)
+        out = weights * x
+        return out
+
+
+class DBox:
+    def __init__(self, cfg: dict):
+        """default boxes
+
+        Args:
+            cfg:
+                - input_size
+                - feature_maps
+                - steps
+                - min_sizes
+                - max_sizes
+        """
+        self.image_size = cfg["input_size"]  # 300
+        self.feature_maps = cfg["feature_maps"]
+        self.num_priors = len(self.feature_maps)
+        self.steps = cfg["steps"]  # [8, 16, ...] DBox pixel size
+        self.min_sizes = cfg["min_sizes"]  # [30, 60, ...] small square pixel size
+        self.max_sizes = cfg["max_sizes"]  # [60, 111, ...]
+        self.aspect_ratios = cfg["aspect_ratios"]
+
+    def make_dbox_list(self):
+        """make dbox"""
+        mean = []
+        # feature_maps: [38, 19, 10, 5, 3, 1]
+        for k, f in enumerate(self.feature_maps):
+            for i, j in product(range(f), repeat=2):
+                # feature size
+                # 300 / steps: [8, 16, 32, 64, 100, 300]
+                f_k = self.image_size / self.steps[k]
+
+                # dbox center
+                cx = (j + 0.5) / f_k
+                cy = (i + 0.5) / f_k
+
+                # aspect ratio 1 small dbox [cx, cy, width, height]
+                # min_sizes: [30, 60, 111, 162, 213, 264]
+                s_k = self.min_sizes[k] / self.image_size
+                mean += [cx, cy, s_k, s_k]
+
+                # aspect ratio 1 large dbox
+                # max_sizes: [60, 111, 162, 213, 264, 315]
+                s_k_prime = sqrt(s_k * (self.max_sizes[k] / self.image_size))
+                mean += [cx, cy, s_k_prime, s_k_prime]
+
+                # other aspect ratios
+                for ar in self.aspect_ratios[k]:
+                    mean += [cx, cy, s_k * sqrt(ar), s_k / sqrt(ar)]
+                    mean += [cx, cy, s_k / sqrt(ar), s_k * sqrt(ar)]
+
+        # dbox to tensor: torch.Size([8732, 4])
+        output = torch.Tensor(mean).view(-1, 4)
+
+        # dbox to [0, 1]
+        output.clamp_(max=1, min=0)
+
+        return output
+
+
+class SSD(nn.Module):
+    def __init__(self, phase: str, cfg: dict):
+        """network
+
+        Args:
+            phase: ['train', 'inference']
+            cfg:
+                - num_classes
+                - bbox_aspect_num
+                - <for_dbox>
+        """
+        super(SSD, self).__init__()
+        self.phase = phase  # ['train', 'inference']
+        self.num_classes = cfg["num_classes"]
+
+        # ssd
+        self.vgg = make_vgg()
+        self.extras = make_extras()
+        self.L2Norm = L2Norm()
+        self.loc, self.conf = make_loc_conf(self.num_classes, cfg["bbox_aspect_num"])
+
+        # dbox
+        dbox = DBox(cfg)
+        self.dbox_list = dbox.make_dbox_list()
+
+        if phase == "inference":
+            self.detect = Detect()
+
+    def forward(self, x):
+        sources = []
+        loc = []
+        conf = []
+
+        # up till vgg conf4_3
+        for k in range(23):
+            x = self.vgg[k](x)
+        sources.append(x)
+
+        # extras
+        for k, v in enumerate(self.extras):
+            x = F.relu(v(x), inplace=True)
+            if k % 2 == 1:
+                sources.append(x)
+        # conv to sources
+        for x, l, c in zip(sources, self.loc, self.conf):
+            loc.append(l(x).permute(0, 2, 3, 1).contiguous())
+            conf.append(c(x).permute(0, 2, 3, 1).contiguous())
+            # contiguous for using view later
+            # l(x), c(x) output is [batch_num, 4 * num_aspects, feature_w, feature_h]
+            # permute to [batch_num, feature_w, feature_h, 4 * num_aspects]
+
+        # loc to [batch_num, 34_928]
+        # conf to [batch_num, 183_372]
+        loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
+        conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
+
+        # loc to [batch_num, 8732, 4]
+        # conf to [batch_num, 8732, 21]
+        loc = loc.view(loc.size(0), -1, 4)
+        conf = conf.view(loc.size(0), -1, self.num_classes)
+
+        output = (loc, conf, self.dbox_list)
+
+        if self.phase == "inference":
+            return self.detect(*output)
+        else:
+            return output
+
+
+def decode(loc, dbox_list):
+    """Using offset, convert dbox to bbox
+
+    Args:
+        loc: [8732, 4] offets from ssd
+        dbox_list: [8732, 4] dbox info
+
+    Returns: boxes [xmin, ymin, xmax, ymax]
+    """
+    # dbox: [cx, cy, width, height]
+    # loc: [d_cx, d_cy, d_width, d_height]
+    boxes = torch.cat(
+        (
+            dbox_list[:, :2] + loc[:, :2] * 0.1 * dbox_list[:, 2:],
+            dbox_list[:, 2:] * torch.exp(loc[:, 2:] * 0.2),
+        ),
+        dim=1,
+    )
+    # becomes: torch.Size([8732, 4])
+
+    # bbox [cx, cy, width, height] -> [xmin, ymin, xmax, ymax]
+    boxes[:, :2] -= boxes[:, 2:] / 2  # to (xmin, ymin)
+    boxes[:, 2:] += boxes[:, :2]  # to (xmax, ymax)
+
+    return boxes
+
+
+def nm_suppression(boxes, scores, overlap=0.45, top_k=200):
+    """Non-Maximum suppression. Remove bboxes with overlap.
+
+    Args:
+        boxes: [確信度0.01を超えたbbox数, 4] bbox
+        scores: [確信度0.01を超えたbbox数] conf
+        overlap:
+        top_k:
+
+    Returns:
+        - keep: list. conf の降順に nms を通過した index が格納
+        - count: int. nms を通過した bbox の数
+    """
+    # return values
+    count = 0
+    keep = scores.new(scores.size(0)).zero_().long()
+    # keep: torch.Size([確信度閾値を超えたbbox数]), all zero
+
+    # 各bbox の面積を計算
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    area = torch.mul(x2 - x1, y2 - y1)
+
+    # copy boxes. use for IOU later
+    tmp_x1 = boxes.new()
+    tmp_y1 = boxes.new()
+    tmp_x2 = boxes.new()
+    tmp_y2 = boxes.new()
+    tmp_w = boxes.new()
+    tmp_h = boxes.new()
+
+    # sort scores (small -> big)
+    v, idx = scores.sort()
+
+    # take top k
+    idx = idx[-top_k:]
+
+    while idx.numel() > 0:
+        i = idx[-1]  # largest conf
+
+        # keep current, remove non-maximum
+        keep[count] = i
+        count += 1
+
+        # break if last
+        if idx.size(0) == 1:
+            break
+
+        # pop idx (we used the largest one)
+        idx = idx[:-1]
+
+        ##############################
+        # remove bbox with large IOU
+        ##############################
+
+        # save variables (up to idx)
+        torch.index_select(x1, 0, idx, out=tmp_x1)
+        torch.index_select(y1, 0, idx, out=tmp_y1)
+        torch.index_select(x2, 0, idx, out=tmp_x2)
+        torch.index_select(y2, 0, idx, out=tmp_y2)
+
+        # clamp to current box
+        tmp_x1 = torch.clamp(tmp_x1, min=x1[i])
+        tmp_y1 = torch.clamp(tmp_y1, min=y1[i])
+        tmp_x2 = torch.clamp(tmp_x2, min=x2[i])
+        tmp_y2 = torch.clamp(tmp_y2, min=y2[i])
+
+        # resize (decrement) w and h
+        tmp_w.resize_as_(tmp_x2)
+        tmp_h.resize_as_(tmp_y2)
+
+        # clamp した状態で bbox の幅と高さを求める
+        tmp_w = tmp_x2 - tmp_x1
+        tmp_h = tmp_y2 - tmp_y1
+
+        # min to 0
+        tmp_w = torch.clamp(tmp_w, min=0.0)
+        tmp_h = torch.clamp(tmp_h, min=0.0)
+
+        inter = tmp_w * tmp_h
+
+        # IOU = intersect / (a + b - intersect)
+        rem_areas = torch.index_select(area, 0, idx)
+        union = (rem_areas - inter) + area[i]
+        IoU = inter / union
+
+        # IoU が overlap より小さい idx のみを残す
+        idx = idx[IoU.le(overlap)]
+        # -> 大きいものが消去される
+
+    return keep, count
+
+
+class Detect(Function):
+    def __init__(self, conf_thresh=0.01, top_k=200, nms_thresh=0.45):
+        """Detection, forward
+
+        Args:
+            conf_thresh:
+            top_k:
+            nms_thresh:
+        """
+        self.softmax = nn.Softmax(dim=-1)  # conf をsoftmax で正規化
+        self.conf_thresh = conf_thresh
+        self.top_k = top_k
+        self.nms_thresh = nms_thresh
+
+    def forward(self, loc_data, conf_data, dbox_list):
+        """順伝播
+
+        Args:
+            loc_data: [batch_num, 8732, 4]
+            conf_data: [batch_num, 8732, num_classes]
+            dbox_list: [8732, 4]
+
+        Returns: output: torch.Size([batch_num, 21, 200, 5])
+            batch_num, class, which top 200, bbox info
+        """
+        # get sizes
+        num_batch = loc_data.size(0)
+        num_dbox = loc_data.size(1)
+        num_classes = conf_data.size(2)
+
+        # softmax on conf
+        conf_data = self.softmax(conf_data)
+
+        # get output template.
+        output = torch.zeros(num_batch, num_classes, self.top_k, 5)
+
+        # resize conf data from [batch_num, 8732, num_classes] to [batch_num, num_classes, 8732]
+        conf_preds = conf_data.transpose(2, 1)
+
+        # minibatch
+        for i in range(num_batch):
+            # 1. loc と dboxから修正した bbox
+            decoded_boxes = decode(loc_data[i], dbox_list)
+
+            # copy conf
+            conf_scores = conf_preds[i].clone()
+
+            # loop fo reach image class (0 is background)
+            for cl in range(1, num_classes):
+                # 2. conf の閾値を超えた bbox を取り出す
+                # conf の閾値を超えているマスクを作成し、
+                # 閾値を超えた conf のインデックスを c_mask として取得
+                c_mask = conf_scores[cl].gt(self.conf_thresh)
+                # conf_scores: [21, 8732]
+                # c_mask: torch.size([8732])
+
+                # scores: torch.Size([閾値を超えたbbox数])
+                scores = conf_scores[cl][c_mask]
+
+                # 閾値を超えた conf がない場合何もしない
+                if scores.nelement() == 0:
+                    continue
+
+                # c_mask を decoded_boxes に適用できるようにサイズ変更
+                l_mask = c_mask.unsqueeze(1).expand_as(decoded_boxes)
+                # l_mask: [8732, 4]
+
+                # l_mask を decoded_boxes に適用
+                boxes = decoded_boxes[l_mask].view(-1, 4)
+                # decoded_boxes[l_mask] で1次元になってしまうので
+                # view で（閾値を超えたbbox数, 4) サイズに変形し直す
+
+                # 3. non-maximum suppression を実施
+                ids, count = nm_suppression(boxes, scores, self.nms_thresh, self.top_k)
+                # ids: confの降順にnms を通過した index
+
+                # outputにnmsを抜けた結果を格納
+                output[i, cl, :count] = torch.cat(
+                    (scores[ids[:count]].unsqueeze(1), boxes[ids[:count]]), 1
+                )
+        return output  # torch.Size([1, 21, 200, 5])
